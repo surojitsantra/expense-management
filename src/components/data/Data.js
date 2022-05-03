452a3367eb2abd46ce1525cdc1d0be86b135cd51
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2022-05-03 13:43:47 +0530

    updated the data

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,21 +1,21 @@
 const Data = [
   {
     id: "1",
-    title: "Toilet Paper",
-    amount: 94.12,
+    title: "Mobile",
+    amount: 9999,
     date: new Date(2020, 7, 14),
   },
   { id: "2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "3",
-    title: "Car Insurance",
-    amount: 294.67,
+    title: "House Rent",
+    amount: 22294.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: "4",
-    title: "New Desk (Wooden)",
-    amount: 450,
+    title: "Laptop",
+    amount: 45000,
     date: new Date(2021, 5, 12),
   },
 ];
